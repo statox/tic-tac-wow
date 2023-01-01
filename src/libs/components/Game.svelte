fdddf9be1cf530da41e772040c3e2dbf518cd84f
Adrien Fabre <adrien.fabre@dashlane.com>
2023-01-01 22:48:40 +0100

    Add turn logic

--- src/libs/components/Game.svelte
+++ src/libs/components/Game.svelte
@@ -6,24 +6,37 @@
     const game = getNewGame();
 
     let selectedPiece: number;
-    let selectedCell: { x: number; y: number };
+    let playerTurn: 1 | 2 = 1;
 
     const onSelectPiece = (piece: number) => {
+        if ((playerTurn === 1 && piece < 0) || (playerTurn === 2 && piece > 0)) {
+            return;
+        }
         selectedPiece = piece;
     };
 
     const onSelectCell = (x: number, y: number) => {
-        selectedCell = { x, y };
-        console.log({ selectedCell });
-        placePieceOnCell(selectedPiece, { x, y }, game);
+        if (selectedPiece === 0) {
+            return;
+        }
+        const result = placePieceOnCell(selectedPiece, { x, y }, game);
+        if (result instanceof Error) {
+            return;
+        }
         game.grid = game.grid;
+        selectedPiece = 0;
+        playerTurn = playerTurn === 1 ? 2 : 1;
     };
 </script>
 
-<span>Hello Game</span>
-
 <div class="d-flex justify-content-center">
     <Board {onSelectCell} grid={game.grid} />
-    <PlayerPieces {onSelectPiece} playerPieces={game.player1Pieces} player={1} />
-    <PlayerPieces {onSelectPiece} playerPieces={game.player2Pieces} player={2} />
+    <span>Player: {playerTurn}</span>
+    {#if playerTurn === 1}
+        <PlayerPieces {onSelectPiece} playerPieces={game.player1Pieces} player={1} />
+        <PlayerPieces {onSelectPiece} playerPieces={game.player2Pieces} player={2} />
+    {:else}
+        <PlayerPieces {onSelectPiece} playerPieces={game.player2Pieces} player={2} />
+        <PlayerPieces {onSelectPiece} playerPieces={game.player1Pieces} player={1} />
+    {/if}
 </div>
