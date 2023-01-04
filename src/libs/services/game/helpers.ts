3be7024c4022ac9e850da357e997d46de073891b
Adrien Fabre <adrien.fabre@dashlane.com>
2023-01-04 19:44:56 +0100

    Add some helper functions

--- src/libs/services/game/helpers.ts
+++ src/libs/services/game/helpers.ts
@@ -1,7 +1,11 @@
 import type { Game } from './types';
 
+export const copyGame = (game: Game): Game => {
+    return JSON.parse(JSON.stringify(game));
+};
+
 export const printGameGrid = (game: Game, message?: string, showFull?: true) => {
-    console.log(message);
+    message && console.log(message);
     if (!showFull) {
         console.log(
             game.grid.map((l) => l.map((ps) => ps.map((p) => p.value)).join('\t|')).join('\n')
@@ -14,4 +18,22 @@ export const printGameGrid = (game: Game, message?: string, showFull?: true) => 
             .map((l) => l.map((ps) => ps.map((p) => `${p.value}-${p.selected}`)).join('\t|'))
             .join('\n')
     );
+};
+
+export const printGameHands = (game: Game, message?: string, showFull?: true) => {
+    message && console.log(message);
+    for (const hand of [game.player1, game.player2]) {
+        if (showFull) {
+            console.log(
+                `P${hand.player}:`,
+                hand.pieces.map((p) => `${p.value}-${p.selected}`),
+                hand.selectedPiece
+            );
+            continue;
+        }
+        console.log(
+            `P${hand.player}:`,
+            hand.pieces.map((p) => p.value)
+        );
+    }
 };
