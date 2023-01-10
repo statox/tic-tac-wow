58b1eeb51f652f1d554bab5a52ee4202e6d83ef1
Adrien Fabre <adrien.fabre@dashlane.com>
2023-01-10 01:25:05 +0100

    Deduplicate hand values in getHandPossibleSelections
    Alphabeta with and without pruning now roughly take the same time
    because the duplicate values were prunned anyway (probably)

--- src/libs/services/game/ia/helpers/getHandPossibleSelection.ts
+++ src/libs/services/game/ia/helpers/getHandPossibleSelection.ts
@@ -4,7 +4,22 @@ import type { Game, PiecePosition, PlayerHand } from '../../types';
 export const getHandPossibleSelection = (game: Game, hand: PlayerHand): PiecePosition[] => {
     const possible: PiecePosition[] = hand.pieces
         .map((_, index) => index)
-        .filter((index) => !hand.unselectableIndexes.has(index))
+        .filter((index) => {
+            // Avoid selecting twice the same value from hand
+            if (hand.unselectableIndexes.has(index)) {
+                return false;
+            }
+            if (index === 1 && !hand.unselectableIndexes.has(0)) {
+                return false;
+            }
+            if (index === 3 && !hand.unselectableIndexes.has(2)) {
+                return false;
+            }
+            if (index === 5 && !hand.unselectableIndexes.has(4)) {
+                return false;
+            }
+            return true;
+        })
         .map((index) => {
             return { index, from: 'hand' };
         });
--- src/tests/game/ia/helpers/getHandPossibleSelections.test.ts
+++ src/tests/game/ia/helpers/getHandPossibleSelections.test.ts
@@ -2,7 +2,7 @@ import { getHandPossibleSelection } from 'src/libs/services/game/ia/helpers';
 import { describe, expect, it } from 'vitest';
 import { convertFixtureGridToGame } from '../../helpers';
 
-describe('getHandsPossibleMoves', () => {
+describe('getHandPossibleSelection', () => {
     it('Should work', () => {
         const game = convertFixtureGridToGame([
             [[2, 3], [2], []],
@@ -15,7 +15,6 @@ describe('getHandsPossibleMoves', () => {
         const p1Selections = getHandPossibleSelection(game, game.player1);
         expect(p1Selections).toStrictEqual([
             { from: 'hand', index: 0 },
-            { from: 'hand', index: 1 },
             { from: 'hand', index: 5 },
             { from: 'board', position: { x: 0, y: 0 } },
             { from: 'board', position: { x: 1, y: 0 } }
@@ -25,7 +24,6 @@ describe('getHandsPossibleMoves', () => {
         expect(p2Selections).toStrictEqual([
             { from: 'hand', index: 1 },
             { from: 'hand', index: 2 },
-            { from: 'hand', index: 3 },
             { from: 'board', position: { x: 2, y: 1 } },
             { from: 'board', position: { x: 0, y: 2 } },
             { from: 'board', position: { x: 2, y: 2 } }
