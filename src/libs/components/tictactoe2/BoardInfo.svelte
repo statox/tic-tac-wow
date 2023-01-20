6bced17918898a7969c0388c96653a6e68991f9b
Adrien Fabre <adrien.fabre@dashlane.com>
2023-01-20 22:43:56 +0100

    Add players binary values to BoardInfo

--- src/libs/components/tictactoe2/BoardInfo.svelte
+++ src/libs/components/tictactoe2/BoardInfo.svelte
@@ -7,6 +7,32 @@
 <div>
     <h4>Board info</h4>
     {#if board}
-        <span>State: {getGameState(board)}</span>
+        <div class="overlay">
+            <span>State: {getGameState(board)}</span>
+            <div class="grid2x2">
+                <span>Player 1 (player, O)</span>
+                <span>Player 2 (computer, X)</span>
+
+                <span>{board.player.toString(2).padStart(9, '0')}</span>
+                <span>{board.computer.toString(2).padStart(9, '0')}</span>
+
+                <span>{board.player}</span>
+                <span>{board.computer}</span>
+            </div>
+        </div>
     {/if}
 </div>
+
+<style>
+    .overlay {
+        display: grid;
+        grid-template-columns: repeat(2, auto);
+        grid-auto-flow: row;
+    }
+    .grid2x2 {
+        display: grid;
+        grid-template-columns: repeat(2, auto);
+        grid-template-rows: repeat(3, auto);
+        grid-auto-flow: row;
+    }
+</style>
