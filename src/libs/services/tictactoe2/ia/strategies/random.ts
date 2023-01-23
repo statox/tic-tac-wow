f26ecf95cfcb11e1d0e2a49f01f4a93a26908bcf
Adrien Fabre <adrien.fabre@dashlane.com>
2023-01-23 22:39:56 +0100

    Add some descriptions to strategies

--- src/libs/services/tictactoe2/ia/strategies/random.ts
+++ src/libs/services/tictactoe2/ia/strategies/random.ts
@@ -1,5 +1,9 @@
 import { getBoardFreeSpots, indexToXY, type Board, type BoardCoord } from '../../game';
 
+/*
+ * This is the most basic strategy to play tictactoe
+ * List all the available spots on the board and choose one randomly
+ */
 export function getMoveRandom(board: Board): BoardCoord {
     const freeSpots = getBoardFreeSpots(board);
 
