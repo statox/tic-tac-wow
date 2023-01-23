9f4197ebf7c96d1ca20d1e5bd2cde390eae6b2bc
Adrien Fabre <adrien.fabre@dashlane.com>
2023-01-23 21:59:58 +0100

    IA helper function to create nodes and deduplicate childs

---
+++ src/libs/services/tictactoe2/ia/moves.ts
@@ -0,0 +1,29 @@
+import { getBoardFreeSpots, indexToXY, makeMoveOnBoard, type Board, type Player } from '../game';
+import { boardAreEquivalent } from './boardRotation';
+
+export function getPossibleMoves(board: Board, player: Player) {
+    const freeSpots = getBoardFreeSpots(board);
+    return freeSpots.map((moveAsIndex) => {
+        const move = indexToXY(moveAsIndex);
+        if (move instanceof Error) {
+            throw move;
+        }
+        const copy = { ...board };
+        makeMoveOnBoard(copy, player, move);
+        return copy;
+    });
+}
+
+export function getUniquePossibleMoves(board: Board, player: Player) {
+    const boards = getPossibleMoves(board, player);
+
+    return boards.reduce((dedupMoves, board) => {
+        for (const existingBoard of dedupMoves) {
+            if (boardAreEquivalent(existingBoard, board)) {
+                return dedupMoves;
+            }
+        }
+        dedupMoves.push(board);
+        return dedupMoves;
+    }, [] as Board[]);
+}
