9ea299844c7d19072500e4c82ec5d8b056e0201f
statox <me@statox.fr>
2023-01-24 00:53:33 +0000

    Deploying to gh-pages from @ statox/tic-tac-wow@f56eefedfc62b67045d8d4329507d4c6a56b11d0 🚀

--- _app/immutable/chunks/6-0d4950b6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/tictactoe2/_page.svelte-87e686d0.js";export{t as component};
---
+++ _app/immutable/chunks/6-71a0c454.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/tictactoe2/_page.svelte-feeda2a0.js";export{t as component};
