3cb3cefaeee677e8d8af3396d7a2634243a80aad
Adrien Fabre <adrien.fabre@dashlane.com>
2023-01-26 01:39:17 +0100

    formatting

--- src/libs/services/tictactoe2/ia/strategies/hardcodedCompleteRules.ts
+++ src/libs/services/tictactoe2/ia/strategies/hardcodedCompleteRules.ts
@@ -24,7 +24,11 @@ import {
  * 8 Win: If the player has two in a row, they can place a third to get three in a row.
  * 7 Block: If the opponent has two in a row, the player must play the third themselves to block the opponent.
  * 6 Fork: Cause a scenario where the player has two ways to win (two non-blocked lines of 2).
- * 5 NOT IMPLEMENTED Blocking an opponent's fork: If there is only one possible fork for the opponent, the player should block it. Otherwise, the player should block all forks in any way that simultaneously allows them to make two in a row. Otherwise, the player should make a two in a row to force the opponent into defending, as long as it does not result in them producing a fork. For example, if "X" has two opposite corners and "O" has the center, "O" must not play a corner move to win. (Playing a corner move in this scenario produces a fork for "X" to win.)
+ * 5 NOT IMPLEMENTED Blocking an opponent's fork:
+ *  - If there is only one possible fork for the opponent, the player should block it.
+ *  - Otherwise, the player should block all forks in any way that simultaneously allows them to make two in a row.
+ *  - Otherwise, the player should make a two in a row to force the opponent into defending, as long as it does not result in them producing a fork.
+ *  - For example, if "X" has two opposite corners and "O" has the center, "O" must not play a corner move to win. (Playing a corner move in this scenario produces a fork for "X" to win.)
  * 4 Center: A player marks the center. (If it is the first move of the game, playing a corner move gives the second player more opportunities to make a mistake and may therefore be the better choice; however, it makes no difference between perfect players.)
  * 3 Opposite corner: If the opponent is in the corner, the player plays the opposite corner.
  * 2 Empty corner: The player plays in a corner square.
