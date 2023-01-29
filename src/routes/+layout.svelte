164e37a2b8b033a8ae49572d248794feafe879d9
Adrien Fabre <adrien.fabre@dashlane.com>
2023-01-29 22:35:23 +0100

    Update newcss theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
-<h1>Tic Tac Wow</h1>
+<header>
+    <h1>Tic Tac Wow</h1>
 
-<nav>
-    <a href="/">Home</a> |
-    <a href="/tictactoe">Tic Tac Toe</a> |
-    <a href="/gobblet">Gobblet</a> |
-    <a href="/misere">Misere</a> |
-</nav>
+    <nav>
+        <a href="/">Home</a> |
+        <a href="/tictactoe">Tic Tac Toe</a> |
+        <a href="/gobblet">Gobblet</a> |
+        <a href="/misere">Misere</a> |
+    </nav>
+</header>
 
 <slot />
